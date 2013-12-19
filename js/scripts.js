866a1720a38f12449a57a87d298cfbd9497b0bcc
Todd Walden <toddjwalden@gmail.com>
2013-12-19 03:12:46 -0600

    nav and item changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   $('.item-cart-buttons .btn-primary').click(function() {
+    $('.item-cart-buttons').hide();
+    $('.item-cart-buttons-added').show();
     $('.nav-cart-empty').hide();
     $('.nav-cart').show();
     $('.nav-cart').addClass('open');
